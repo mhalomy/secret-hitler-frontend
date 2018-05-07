8483cf312400082d02231fa31a6f1c10e96c71a9
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-07 13:00:34 +0200

    feat: add redux and socket reducer

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import reducers from './redux/reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers, applyMiddleware(logger))}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
---
+++ redux/reducers/initialAppState.js
@@ -0,0 +1,6 @@
+export default {
+  socket: {
+    backendIp: '',
+    backendPort: '',
+  }
+}
