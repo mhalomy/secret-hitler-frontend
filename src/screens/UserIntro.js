4d66c780ec8e13def2f7c15857e44fd974b3b5d7
DiegoAHV <dahvdiego@gmail.com>
2018-05-07 16:26:13 +0200

    feat: add userintro

---
+++ src/screens/UserIntro.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+// import Card from '../components/Card';
+// import CardSection from '../components/CardSection';
+
+
+class UserIntro extends Component {
+
+  render() {
+    let roleImage =
+    this.props.role === 'liberal' ? require('../assets/liberal.jpg') :
+    this.props.role === 'fascist' ? require('../assets/liberal.jpg') :
+    require('../assets/trump.jpg')
+    return (
+    <View style={styles.parent}>
+      {/* DO WE WANT ANY TEXT IN HERE??? <Text>Hello World</Text> */}
+
+      <Image
+        source={roleImage}
+        style={styles.imageStyle}
+      />
+    </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  parent: {
+    marginTop:20,
+    backgroundColor: 'grey',
+    flex:1,
+    flexDirection: 'column',
+  },
+  imageStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 400,
+    width:null,
+    flex: 1
+  },
+});
+
+export default UserIntro;
