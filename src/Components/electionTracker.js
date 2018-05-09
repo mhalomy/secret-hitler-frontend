9b38bbbcd2ca4024dfc812edb58797a76dbcae5b
lululynx <ldmjoncourt@gmail.com>
2018-05-09 12:24:38 +0200

    feat: start implementation of  board screen and components

---
+++ src/Components/electionTracker.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button  } from 'react-native';
+
+
+
+export default class ElectionTracker extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  renderTrackers = () => {
+    const trackers = [];
+    for (let i = 0; i < 4; i++) {
+      //circleArray.push(<View key={i} style={styles.circle}/>)
+      trackers.push(<Image key={i} source={require('../assets/board/trackerCircle.png')} width={'100%'} flex={0.5} margin={'5%'}/>)
+    }
+    return trackers;
+  }
+
+  render () {
+    return (
+      <View style={styles.electionTracker}>
+        {this.renderTrackers()}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  electionTracker: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    justifyContent: 'space-around',
+    marginRight: 15,
+  },
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 100/2,
+    backgroundColor: 'orange',
+    borderStyle: 'solid',
+    margin: '15%',
+  }
+});
---
+++ src/Screens/mainBoard.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import ElectionTracker from '../Components/electionTracker';
+import Board from '../Components/board';
+
+export default class MainBoard extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <View style={styles.mainBoardContainer}>
+        <View style={styles.allianceBoards}>
+          <View style={styles.liberalBoard}>
+            <ImageBackground source={require('../assets/board/liberalBoard.png')} style={{flex:1}} >
+              <Board className='liberal'/>
+            </ImageBackground>
+          </View>
+          <View style={styles.fascistBoard}>
+            <ImageBackground source={require('../assets/board/fascistBoard.png')} style={{flex:1}} >
+              <Board className='fascist'/>
+            </ImageBackground>
+          </View>
+        </View>
+        <View style={styles.electionTracker}>
+          <ElectionTracker />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  mainBoardContainer : {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100%',
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+
+  electionTracker: {
+    alignSelf: 'center',
+    flex: 1,
+  },
+
+  allianceBoards: {
+    display: 'flex',
+    flex: 8,
+  },
+
+  liberalBoard : {
+    backgroundColor: 'white',
+    height: '50%',
+  },
+
+  fascistBoard : {
+    height: '50%',
+    backgroundColor: 'white',
+  },
+})
