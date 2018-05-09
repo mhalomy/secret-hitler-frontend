2185c16dc7071865c37bd9b5590af6eecbdb268c
lululynx <ldmjoncourt@gmail.com>
2018-05-09 12:41:49 +0200

    feat: update election tracker component

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import UserIntro from './src/screens/UserIntro';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import reducers from './redux/reducers';
 
 export default class App extends React.Component {
   render() {
+    const store = createStore(reducers);
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers, applyMiddleware(logger))}>
+        <UserIntro/>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
