e015d20636f645559ad8a1b23154f942f507e1b6
lululynx <ldmjoncourt@gmail.com>
2018-05-11 19:33:51 +0200

    feat: socketIo middleware and modified actions

--- App.js
+++ App.js
@@ -1,26 +1,5 @@
-// import React from 'react';
-// import UserIntro from './src/screens/UserIntro';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
-//
-// import reducers from './redux/reducers';
-//
-// export default class App extends React.Component {
-//   render() {
-//     const store = createStore(reducers);
-//     return (
-//       <Provider store={createStore(reducers, applyMiddleware(logger))}>
-//         <UserIntro/>
-//       </Provider>
-//     );
-//   }
-// }
-
 import React from 'react';
 
-=======
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -38,8 +17,6 @@ import WaitingRoom from './src/screens/WaitingRoom';
 export default class App extends React.Component {
 
   render() {
-
-=======
     const MainNavigator = createBottomTabNavigator({
       Login: { screen: CreateUser },
       CreateJoin: { screen: CreateJoin },
